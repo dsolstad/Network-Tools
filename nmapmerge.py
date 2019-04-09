88a141ad17dbba16379da7ad3168eac67cc9e7f8
Daniel Solstad <solstad.daniel@gmail.com>
2019-04-09 18:39:23 +0200

    Create nmapmerge.py

---
+++ nmapmerge.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+##
+## A tool to merge multiple nmap scans into CSV
+## Author: Daniel Solstad (dsolstad.com)
+##
+
+import sys
+
+if not sys.version_info[0] == 3:
+    print ("You need to run this with python3")
+    sys.exit()
+
+import os
+import re
+from termcolor import colored
+
+help = """
+nmapmerge.py <path/to/folder>
+
+The script expects will recursivly find all .nmap files from the target path and print out the merged result in CSV. 
+"""
+
+if len(sys.argv) != 2:
+    print (help)
+    sys.exit(1)
+
+rootfolder = sys.argv[1]
+results = []
+
+for subdir, dirs, files in os.walk(rootfolder):
+    for file in files:
+
+        filename = os.path.join(subdir, file)
+        if not filename.endswith('.nmap'): continue
+
+        for scan in open(filename, 'r').read().split("\n\n"):
+
+            info = {}
+            ipaddr = re.findall(r'Nmap scan report for (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', scan)
+            ports = re.findall(r'(\d+)\/(tcp|udp)\s+(.*?)\s+(.*?)\s+(.*?)', scan)
+
+            for key, val in enumerate(ports):
+                info['ipaddr'] = ipaddr[0]
+                info['port'] = val[0]
+                info['protocol'] = val[1]
+                info['state'] = val[2]
+                info['service'] = val[3]
+                info['version'] = val[4]
+                results.append(dict(info))
+
+try:
+    # Print CSV headers
+    for item in results[0].keys():
+        print(item + ',', end='')
+
+    # Print CSV values
+    for x in results:
+        print("\n", end='')
+        for key, value in x.items():
+            print(value, end='')
+            print(',', end='')
+    print("")
+except: pass
