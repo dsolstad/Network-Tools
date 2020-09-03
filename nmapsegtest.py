845ed3ec52c4e91242511639ed49d3c9a7bc1bbe
Daniel Solstad <solstad.daniel@gmail.com>
2020-09-03 14:39:11 +0200

    Update nmapsegtest.py

--- nmapsegtest.py
+++ nmapsegtest.py
@@ -3,20 +3,21 @@
 ## Simple segmentation scan
 ## Author: Daniel Solstad (dsolstad.com)
 ##
-## 1. Get all unique ports from all nmap scans with nmapuniqueports.py. 
+## 1. Add '-p-' as a parameter to scan all 65k ports,
+##    or get all unique ports from previous nmap scans with nmapuniqueports.py. 
 ##    $ python3 nmapuniqueports.py /path/to/previous/results > ports.txt
 ##
 ## 2. Write target subnets in targets.txt (seperated with newlines), then run:
 ##    cat targets.txt | xargs -I CMD -P 3 python3 nmapsegtest.py CMD
 ##    This will run three nmap processes in parallel at all times.
 ##    Increase/decrease accordingly to your network load.
+##    The real increase in speed is with --min-rate=x, but use with caution. Try e.g. with 1000 first.
 ##
 ## 3. Move the results to a subfolder named e.g. after the source subnet. Example structure:
 ##    /segtest/Results/Source_VLAN_101
 ##    /segtest/Results/Source_VLAN_102
 ##
-## 4. Run $ python3 nmapmerge.py /segtest/Results/Source_VLAN_101 to 
-##     view all potential openings from that VLAN.
+## 4. Run $ python3 nmapmerge.py /segtest/Results/Source_VLAN_101 to view all potential openings from that VLAN.
 ##
 
 import sys
