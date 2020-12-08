709a32c585306b8d6d651b46dcd54896f02833b1
Daniel Solstad <solstad.daniel@gmail.com>
2020-12-08 09:47:12 +0100

    Update nmapsegtest.py

--- nmapsegtest.py
+++ nmapsegtest.py
@@ -3,21 +3,24 @@
 ## Simple segmentation scan
 ## Author: Daniel Solstad (dsolstad.com)
 ##
-## 1. Add '-p-' as a parameter to scan all 65k ports (uncomment ports variable),
+## 1. Chose ports
+##    Add '-p-' as a parameter to scan all 65k ports (uncomment ports variable),
 ##    or, to speed up the test, get all unique ports from previous nmap scans with:
 ##    $ grep -Er '^[0-9]{1,6}\/[tcp|udp]' Results/ | grep open | cut -d':' -f2 | cut -d'/' -f1 | sort -n | uniq | tr '\n' ',' > ports.txt
 ##
-## 2. Write target subnets in targets.txt (seperated with newlines), then run:
+## 2. Add targets and start scan
+##    Write target subnets in targets.txt (nmap syntax), seperated with newlines, then run:
 ##    cat targets.txt | xargs -I CMD -P 3 python3 nmapsegtest.py CMD
-##    This will run three nmap processes in parallel at all times.
-##    Increase/decrease accordingly to your network load.
-##    The real increase in speed is with --min-rate=x, but use with caution. Try e.g. with 1000 first.
+##    This will run three nmap processes in parallel at all times. Increase/decrease -P accordingly to your network load.
+##    You can further add --min-rate=x to the nmap command to speed it up further, but use with caution. Try e.g. with 1000 first.
 ##
-## 3. Move the results to a subfolder named e.g. after the source subnet. Example structure:
+## 3. Process results
+##    Move the results to a subfolder named e.g. after the source subnet. Example structure:
 ##    /segtest/Results/Source_VLAN_101
 ##    /segtest/Results/Source_VLAN_102
 ##
-## 4. Run $ python3 nmapmerge.py /segtest/Results/Source_VLAN_101 to view all potential openings from that VLAN.
+## 4. View results
+##    Run $ python3 nmapmerge.py /segtest/Results/Source_VLAN_101 to view all potential openings from that VLAN.
 ##
 
 import sys
@@ -33,14 +36,18 @@ import subprocess
 target = sys.argv[1]
 target = target.replace("\n", "")
 
+if '#' in target:
+    print ("Skipping commented out line: " + target)
+    sys.exit()
+    
 if '/' in target:
     results_dir = './Results/' + target.replace('/', '[') + ']/'
 else:
     results_dir = './Results/' + target + '/'
 
 if os.path.exists(results_dir):
-    print ("Already scanned " + target)
-    sys.exit(0) # Target already scanned
+    print ("Skipping - already scanned: " + target)
+    sys.exit()
 else:
     os.makedirs(results_dir)
 
@@ -53,6 +60,7 @@ cmd = ['nmap', '-sT', target, '-T4', '-n', '-v', '-Pn', '--reason', '-p', ports,
        '--initial-rtt-timeout=200ms',
        '-oA', results_dir + 'scan']
 
+print ("Starting scan against: " + target)
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 for line in p.stdout:
