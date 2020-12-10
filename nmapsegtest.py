53fc2083435779eef8738211797abd2b321bfd11
Daniel Solstad <solstad.daniel@gmail.com>
2020-12-10 12:50:27 +0100

    Update nmapsegtest.py

--- nmapsegtest.py
+++ nmapsegtest.py
@@ -3,17 +3,19 @@
 ## Simple segmentation scan
 ## Author: Daniel Solstad (dsolstad.com)
 ##
-## 1. Chose ports
-##    By default, the script scans all 65k ports.
-##    You can speed up the scan by only scanning unique ports previously seen in the same network with.
-##    Run the following command against a folder with all the nmap results:
+## Important! Always start slower and then adjust the speed according to what the target network can handle. This can potentially bring a complete network down.
+##
+## 1. Choose tcp or udp and ports
+##    By default, the script scans all 65k tcp ports. Change cmd_tcp to cmd_udp on in the Popen function to scan UDP instead.
+##    You can speed up the scan by only scanning unique ports previously seen in the same network with the following command:
 ##    $ grep -Er '^[0-9]{1,6}\/[tcp|udp]' Results/ | grep open | cut -d':' -f2 | cut -d'/' -f1 | sort -n | uniq | tr '\n' ',' > ports.txt
 ##
 ## 2. Add targets and start scan
 ##    Write target subnets in targets.txt (nmap syntax), seperated with newlines, then run:
-##    cat targets.txt | xargs -I CMD -P 3 python3 nmapsegtest.py CMD
-##    This will run three nmap processes in parallel at all times. Increase/decrease -P accordingly to your network load.
-##    You can further add --min-rate=x to the nmap command to speed it up further, but use with caution. Try e.g. with 1000 first.
+##    cat targets.txt | xargs -I CMD -P 1 python3 nmapsegtest.py CMD
+##    This will run one nmap processes for each target consecutively. This can be increased if the target network can handle it.
+##    Scanning multiple single hosts will usually benefit more by multiple nmap instances, 
+##      since nmap already does a good job optimizing when scanning a whole subnet.
 ##    This script use full connect scan (-sT) to be more friendly towards firewalls, so that they don't keep the connections open.
 ##
 ## 3. Process results
@@ -39,7 +41,7 @@ target = sys.argv[1]
 target = target.replace("\n", "")
 
 if '#' in target:
-    print ("Skipping commented out line: " + target)
+    print ("[+] Skipping commented out line: " + target)
     sys.exit()
     
 if '/' in target:
@@ -48,7 +50,7 @@ else:
     results_dir = './Results/' + target + '/'
 
 if os.path.exists(results_dir):
-    print ("Skipping - already scanned: " + target)
+    print ("[+] Skipping already scanned: " + target)
     sys.exit()
 else:
     os.makedirs(results_dir)
@@ -57,15 +59,22 @@ ports = '1-65535'
 if os.path.exists('./ports.txt'):
     ports = open('./ports.txt','r').read().replace('\r', '').replace('\n', ',')
 
-cmd = ['nmap', '-sT', target, '-T4', '-n', '-v', '-Pn', '--reason', '-p', ports,
+# Be very careful and adjust the min-rate according to the target network.
+cmd_tcp = ['nmap', '-sT', target, '-T4', '-n', '-v', '-Pn', '--reason', '-p', ports,
        '--max-retries=1',
        '--max-scan-delay=1',
+       '--min-rate=1000',
        '--max-rtt-timeout=200ms',
        '--initial-rtt-timeout=200ms',
        '-oA', results_dir + 'scan']
 
-print ("Starting scan against: " + target)
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# We can't scan that fast against UDP ports or the results will be very unaccurate.
+# Consider adding version scan (-sUV) for more accurate results at the cost of speed.
+cmd_udp = ['nmap', '-sU', target, '-T3', '-n', '-v', '-Pn', '--reason', '--top-ports', '100',
+       '-oA', results_dir + 'scan']
+
+print ("[+] Starting scan against: " + target)
+p = subprocess.Popen(cmd_tcp, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 for line in p.stdout:
     line = line.decode('ascii')
